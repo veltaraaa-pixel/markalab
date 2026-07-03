01bbd6642db63b004cef2efeb99782ea7f30a99f
Ludwig Trujillo <veltaraaa@gmail.com>
2026-07-03 13:24:53 -0600

    Update contact information in metadata

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -4,7 +4,7 @@ import ContactoContenido from '@/components/contacto/ContactoContenido'
 export const metadata: Metadata = {
   title: 'Contacto',
   description:
-    'Contáctanos en MarkaLab. Estamos en Jardines del Pedregal, CDMX. Llámanos al +52 55 8100 1579 o escríbenos a comercial@markalab.com.mx',
+    'Contáctanos en MarkaLab. Estamos en Jardines del Pedregal, CDMX. Llámanos al +52 56 1123 2604 o escríbenos a comercial@markalab.com.mx',
 }
 
 export default function ContactoPage() {
